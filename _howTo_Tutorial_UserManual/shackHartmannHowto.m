ca4aa1c05891bcda8f78df3b79e397ae335a85c0
cmcorreias <cmcorreias@gmail.com>
2019-12-05 19:07:46 -1000

    Updated to run with the newest OOMAO shackHartmann calls. Added examples with Nyquist and sub-Nyquist sampled spots.

--- _howTo_Tutorial_UserManual/shackHartmannHowto.m
+++ _howTo_Tutorial_UserManual/shackHartmannHowto.m
@@ -1,22 +1,33 @@
 %% SHACKHARTMANN HOWTO 
 % Demonstrate the use of the <matlab:doc('shackHartmann') shackHartmann> class
 
-%% shackHartmann definition
-wfs = shackHartmann(20,120,0.75);
-setValidLenslet(wfs)
-tel = telescope(8,'resolution',120);
 ngs = source;
-% wfs.lenslets.lightSource = ngs;
-% relay(tel,ngs);
-ngs = ngs.*tel*wfs.lenslets;
-grabAndProcess(wfs)
-wfs.referenceSlopes = wfs.slopes;
-dataProcessing(wfs)
-figure
-slopesAndFrameDisplay(wfs)
+tel = telescope(8,'resolution',120);
+
+%% A Nyquist-sampled SH :: case 1: pixelSize = lambda/D/2
+% instantiation
+wfs = shackHartmann(20,120,0.5);
+% initialisation
+ngs = ngs.*tel*wfs;
+wfs.INIT
+
+
 wfs.camera.frameListener.Enabled = true;
 wfs.slopesListener.Enabled = true;
-% dataProcessing(wfs)
+
+ngs = ngs.*tel*wfs;
+figure(212)
+imagesc(wfs.camera)
+
+% gain calibration :: default lambda/2/d units
+wfs.gainCalibration(tel,ngs); 
+
+binFactor = max(1,2*wfs.lenslets.fieldStopSize/wfs.lenslets.nyquistSampling/(wfs.camera.resolution(1)/wfs.lenslets.nLenslet));
+lo2DInMas = ngs.wavelength/(2*tel.D/wfs.lenslets.nLenslet)*constants.radian2mas;
+loDInMas = ngs.wavelength/(tel.D/wfs.lenslets.nLenslet)*constants.radian2mas;
+pixelSizeInMas = lo2DInMas*binFactor;
+fprintf('Pixel size is %4.0f mas\n' , pixelSizeInMas)
+fprintf('Pixel size is %4.2f lambda/D \n' , pixelSizeInMas/loDInMas)
 
 %% a random aberration
 zern = zernike(5:6,'resolution',120,'pupil',tel.pupil);
@@ -33,3 +44,96 @@ for k=1:100
     drawnow
 end
 
+
+%% case of a subsampled SH :: case 2: pixelSize = lambda/D
+wfs = shackHartmann(20,120,0.5);
+
+% fieldStopSize is in units of the DL (=lambda/D). By choosing 6, 12 pixels 
+% are used in the Fraunhofer propagation (because
+% wfs.lenslets.nyquistSampling=1 by default). Since the detector only has 6
+% pixels per subaperture, the pixelSize=lambda/D
+ 
+wfs.lenslets.fieldStopSize = 6;
+% initialisation
+ngs = ngs.*tel*wfs;
+wfs.INIT
+
+
+wfs.camera.frameListener.Enabled = true;
+wfs.slopesListener.Enabled = true;
+
+ngs = ngs.*tel*wfs;
+figure(212)
+imagesc(wfs.camera)
+
+binFactor = max(1,2*wfs.lenslets.fieldStopSize/wfs.lenslets.nyquistSampling/(wfs.camera.resolution(1)/wfs.lenslets.nLenslet));
+lo2DInMas = ngs.wavelength/(2*tel.D/wfs.lenslets.nLenslet)*constants.radian2mas;
+loDInMas = ngs.wavelength/(tel.D/wfs.lenslets.nLenslet)*constants.radian2mas;
+pixelSizeInMas = lo2DInMas*binFactor;
+fprintf('Pixel size is %4.0f mas\n' , pixelSizeInMas)
+fprintf('Pixel size is %4.2f lambda/D \n' , pixelSizeInMas/loDInMas)
+
+
+%% case of a subsampled SH :: case 3: pixelSize = 2*lambda/D
+wfs = shackHartmann(20,120,0.5);
+
+% fieldStopSize is in units of the DL (=lambda/D). By choosing 6, 12 pixels 
+% are used in the Fraunhofer propagation (because
+% wfs.lenslets.nyquistSampling=1 by default). Since the detector only has 3
+% pixels per subaperture, the pixelSize=2*lambda/D
+wfs.lenslets.fieldStopSize = 6;
+
+% camera resolution can be set directly in the wfs =
+% shackHartmann(20,60,0.5); It controls the binning of the pixels
+wfs.camera.resolution = 60;
+% initialisation
+ngs = ngs.*tel*wfs;
+wfs.INIT
+
+
+wfs.camera.frameListener.Enabled = true;
+wfs.slopesListener.Enabled = true;
+
+ngs = ngs.*tel*wfs;
+figure(212)
+imagesc(wfs.camera)
+
+binFactor = max(1,2*wfs.lenslets.fieldStopSize/wfs.lenslets.nyquistSampling/(wfs.camera.resolution(1)/wfs.lenslets.nLenslet));
+lo2DInMas = ngs.wavelength/(2*tel.D/wfs.lenslets.nLenslet)*constants.radian2mas;
+loDInMas = ngs.wavelength/(tel.D/wfs.lenslets.nLenslet)*constants.radian2mas;
+pixelSizeInMas = lo2DInMas*binFactor;
+fprintf('Pixel size is %4.0f mas\n' , pixelSizeInMas)
+fprintf('Pixel size is %4.2f lambda/D \n' , pixelSizeInMas/loDInMas)
+
+
+%% case of a subsampled SH :: case 4: quadCell 
+wfs = shackHartmann(20,40,0.5);
+
+% fieldStopSize is in units of the DL (=lambda/D). By choosing 6, 12 pixels 
+% are used in the Fraunhofer propagation (because
+% wfs.lenslets.nyquistSampling=1 by default). Since the detector only has 6
+% pixels per subaperture, the pixelSize=lambda/D
+wfs.lenslets.fieldStopSize = 6; % user can change this from 1 to 6 and check results
+
+% camera resolution can be set directly in the wfs =
+% shackHartmann(20,60,0.5); It controls the binning of the pixels
+% wfs.camera.resolution = 40;
+
+% initialisation
+ngs = ngs.*tel*wfs;
+wfs.INIT
+
+
+wfs.camera.frameListener.Enabled = true;
+wfs.slopesListener.Enabled = true;
+
+ngs = ngs.*tel*wfs;
+figure(212)
+imagesc(wfs.camera)
+
+binFactor = max(1,2*wfs.lenslets.fieldStopSize/wfs.lenslets.nyquistSampling/(wfs.camera.resolution(1)/wfs.lenslets.nLenslet));
+lo2DInMas = ngs.wavelength/(2*tel.D/wfs.lenslets.nLenslet)*constants.radian2mas;
+loDInMas = ngs.wavelength/(tel.D/wfs.lenslets.nLenslet)*constants.radian2mas;
+pixelSizeInMas = lo2DInMas*binFactor;
+fprintf('Pixel size is %4.0f mas\n' , pixelSizeInMas)
+fprintf('Pixel size is %4.2f lambda/D \n' , pixelSizeInMas/loDInMas)

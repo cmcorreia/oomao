e53d454fcff74191fef8516cdf4888b97a7e1dd6
ccorreia <carlos.correia@lam.fr>
2021-01-29 13:57:41 +0000

    Fixed syntax by adding comas where needed

--- _external/cog.m
+++ _external/cog.m
@@ -1,9 +1,9 @@
-function [cx cy] = cog(a)
+function [cx, cy] = cog(a)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 dx = numel(a(:,1));
 dy = numel(a(1,:));
-[X Y] = meshgrid(1:dx, 1:dy);
+[X, Y] = meshgrid(1:dx, 1:dy);
 
 cx = sum(sum(a.*X))/sum(sum(a)) - 1;  % consistant with centroiding (index 0 Npixel - 1)
 cy = sum(sum(a.*Y))/sum(sum(a)) - 1;  % consistant with centroiding (index 0 Npixel - 1)

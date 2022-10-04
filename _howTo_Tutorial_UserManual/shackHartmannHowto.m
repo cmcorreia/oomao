10f01bdcbcd740491acf5736698dad2dc57e7099
ccorreia <carlos.correia@lam.fr>
2022-10-04 11:51:20 +0100

    Fixes to the description and telescope resolution for illustration only

--- _howTo_Tutorial_UserManual/shackHartmannHowto.m
+++ _howTo_Tutorial_UserManual/shackHartmannHowto.m
@@ -3,12 +3,12 @@
 
 %{
 The detector pixel size can be adjusted considering the following
-- The FoV of a sub-aperture is at most 2*nPixElecField*lo2D, where
+- The FoV of a sub-aperture is at most nPixElecField*loD, where
     - nPixElecField = number of pixels per sub-aperture used to sample the
-    electric field. This is tel/resolution/wfs.nLenslet
-    - lo2D is half the diffraction limit of the sub-aperture
+    electric field. This is tel.resolution/wfs.nLenslet
+    - loD is the diffraction limit of the sub-aperture
 - the wfs.lenslets.fieldStopSize the lenslet field of view given in diffraction fwhm units
-- wfs.lenslets.nyquistSampling gives the 2x number of pixels per lo2D; ontrols the angular size of the electric-field pixel
+- wfs.lenslets.nyquistSampling gives the 2x number of pixels per loD; ontrols the angular size of the electric-field pixel
 - wfs.camera.resolution bins the electric field pixels to a smaller number; controls the binning of the detector camera pixels
 
 Example 1: Detector with 8 50mas pixels in K band
@@ -23,6 +23,8 @@ Example 1: Detector with 8 50mas pixels in K band
     is obtained from 10mas pixels -> 50 mas pixels as desired
 %}
 %%
+ccc
+
 ngs = source;
 nL = 1;
 res = 24;
@@ -31,12 +33,18 @@ tel = telescope(8,'resolution',24*nL);
 %% A Nyquist-sampled SH :: case 1: pixelSize = lambda/D/2
 % instantiation
 wfs = shackHartmann(nL,24*nL,0.5);
+
+% 
+wfs.lenslets.nyquistSampling = 1;
+wfs.lenslets.fieldStopSize = 24;
+%wfs.camera.resolution = [48,48]*2;
+fprintf('Max fieldStopSize is : %2.0f \n', 2*tel.resolution/(wfs.lenslets.nyquistSampling*2))
+
+2*wfs.lenslets.nyquistSampling * wfs.lenslets.fieldStopSize
 % initialisation
 ngs = ngs.*tel*wfs;
 wfs.INIT
 
-%wfs.lenslets.fieldStopSize = 24;
-%wfs.camera.resolution = [4,4];
 
 wfs.camera.frameListener.Enabled = true;
 wfs.slopesListener.Enabled = true;
@@ -45,6 +53,7 @@ ngs = ngs.*tel*wfs;
 figure(212)
 imagesc(wfs.camera)
 
+%%
 % gain calibration :: default lambda/2/d units
 wfs.gainCalibration(tel,ngs); 
 
@@ -62,7 +71,7 @@ wfs.lenslets.pixelScale(ngs,tel)
 
 
 %% a random aberration
-zern = zernike(5:6,'resolution',120,'pupil',tel.pupil);
+zern = zernike(5:6,'resolution',tel.resolution,'pupil',tel.pupil);
 zern.c = 20*(2*rand(zern.nMode,1)-1);
 % reset(ngs)*tel*zern*wfs.lenslets;
 % grabAndProcess(wfs)
@@ -80,10 +89,10 @@ end
 %% case of a subsampled SH :: case 2: pixelSize = lambda/D
 % done in two ways
 %   1) set wfs.lenslets.nyquistSampling = 0.5; if done alone, limits the
-%   FoV to half the default value, generating a detector image with hald
+%   FoV to half the default value, generating a detector image with half
 %   the number of pixels
 %       1.1) enlarge the FoV to have the same output detector pixels: wfs.lenslets.fieldStopSize = 6
-%   2) set wfs.lenslets.fieldStopSize = 6; Since the pixel size if 1/2 loD,
+%   2) set wfs.lenslets.fieldStopSize = 6; Since the pixel size is 1/2 loD,
 %   the total FoV=12loD. As the detector resolution is still 6, the
 %   detector will bin the pixels 2x, creating the desired effect
 %
@@ -135,6 +144,7 @@ fprintf('Pixel size is %4.2f lambda/D \n' , pixelSizeInMas/loDInMas)
 %alternative
 wfs.lenslets.pixelScale(ngs,tel)
 %% case of a subsampled SH :: case 3: pixelSize = 2*lambda/D
+tel = telescope(8,'resolution',120);
 wfs = shackHartmann(20,120,0.5);
 
 % fieldStopSize is in units of the DL (=lambda/D). By choosing 6, 12 pixels 

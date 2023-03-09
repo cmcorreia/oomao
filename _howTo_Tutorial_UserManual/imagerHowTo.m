0596655857d55d31acbf3ccc3c7d6572a5ca2d35
ccorreia <carlos.correia@lam.fr>
2023-03-09 13:39:51 +0000

    updated howTo to compare ensquared values from numerical integration on the imager class to analytically integrated values (e.g. https://web.ipac.caltech.edu/staff/fmasci/home/astro_refs/PSFtheory.pdf).

--- _howTo_Tutorial_UserManual/imagerHowTo.m
+++ _howTo_Tutorial_UserManual/imagerHowTo.m
@@ -1,6 +1,6 @@
 %% IMAGER HOWTO
 % Demonstrate the use of the <matlab:doc('imager') detector> class
-
+ccc
 %% ATMOSPHERE
 atm = atmosphere(photometry.V,0.15,30,...
     'altitude',[0,4,10]*1e3,...
@@ -20,7 +20,7 @@ tel = telescope(D,...
 
 % H-science camera
 scienceH = source('wavelength',photometry.H);
-camH = imager('diameter',tel.D, 'fieldStopSize',30,'nyquistSampling',8*2);
+camH = imager('diameter',tel.D, 'fieldStopSize',30,'nyquistSampling',2);
 tel = tel - atm;
 scienceH = scienceH.*tel*camH;
 figure(31416)
@@ -28,26 +28,40 @@ imagesc(camH,'parent',subplot(2,1,1))
 
 camH.referenceFrame = camH.frame;
 
-%% ENSQUARED ENERGY
-nPts = 150;
-pixSizeInArcsec = scienceH.wavelength/tel.D/camH.imgLens.nyquistSampling/2*constants.radian2arcsec;
-camH.eeWidth = linspace(0,60*pixSizeInArcsec,nPts);% integration box size in arcsec
+%% EE in \lambda/D 
+% Encircled (not ensquared) energy of an Airy pattern, from https://web.ipac.caltech.edu/staff/fmasci/home/astro_refs/PSFtheory.pdf
+% ? (?/D units) EE(?)
+% 0.52    0.50
+% 0.70    0.70
+% 1.22    0.86
+% 1.66    0.90
+
+camH.eeWidth = camH.imgLens.nyquistSampling*2* [0.52 0.79 1.22 1.66]';
 camH.flush
-camH.ee(1) = 0;
-xx = linspace(0,60*pixSizeInArcsec,nPts);
+camH.ee(1,:)
+%% ENSQUARED ENERGY
+intBoxDiam = linspace(0,96,50); % in pixels
+camH.eeWidth = intBoxDiam;
+camH.flush
+
+pixSizeInArcsec = scienceH.wavelength/tel.D/camH.imgLens.nyquistSampling/2*constants.radian2arcsec;
+difLimInArcSec = scienceH.wavelength/tel.D*constants.radian2arcsec;
 
 figure
-difLimInArcSec = scienceH.wavelength/tel.D*constants.radian2arcsec;
-plot(xx/difLimInArcSec, squeeze(camH.ee))
-title('Ensquared energy, Airy function')
+plot(intBoxDiam*pixSizeInArcsec/difLimInArcSec, squeeze(camH.ee(1,:)),'displayName','numerical integration in OTF space')
+title(['Ensquared energy, Airy function, sampling: \theta_{pix}=' num2str(camH.imgLens.nyquistSampling/2) '\lambda/D'])
 xlabel('integration box diameter size, [\lambda/D units]')
-set(gca,'FontSize',13)
+set(gca,'FontSize',16)
 grid on
 hold on
+
+pbaspect([1.618 1 1])
 %% NUMERICAL INTEGRATION
+nPts = size(camH.frame,1)/2;
 totalSumRefImage = sum(camH.referenceFrame(:));
 for d = 0:nPts-2
     roi = camH.referenceFrame(end/2-d:end/2+1+d,end/2-d:end/2+1+d);
     ee(d+2) = sum(roi(:))/totalSumRefImage;
 end
-plot(xx/difLimInArcSec*2, ee) % the factor of 2 here is due to the integration box being n x "2" x pixSizeInArcsec
+plot((0:nPts-1)*pixSizeInArcsec/difLimInArcSec, ee,'--', 'displayName','numerical integration in PSF space') 
+legend
